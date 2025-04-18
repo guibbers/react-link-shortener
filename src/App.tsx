9d977176783578107e8cb0ea5816b7003b9af209
Guilherme Torres <guiatorres8@gmail.com>
2025-04-18 02:57:42 -0300

    add shortening feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,34 @@ function App() {
 
 	function getOriginalURL(event: React.ChangeEvent<HTMLInputElement>) {
 		setOriginalURL(event.target.value);
-		console.log(event.target.value);
+	}
+
+	async function handleShortenLink(e: Event) {
+		e.preventDefault();
+		const url = originalURL;
+		const serverURL = "http://localhost:3333/";
+
+		try {
+			const response = await fetch(serverURL, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+
+				body: JSON.stringify({
+					url: url,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error(`${response.status}`);
+			}
+
+			const resultUrl = await response.json();
+			setShortenedURL(resultUrl.shortenedURL);
+		} catch (error) {
+			console.error(error.message);
+		}
 	}
 
 	return (
@@ -19,13 +46,13 @@ function App() {
 			<main>
 				<div className="mainContainer">
 					<div className="linkFormContainer">
-						<form className="linkForm">
+						<form className="linkForm" onSubmit={handleShortenLink}>
 							<input
 								type="text"
 								placeholder="Insira sua URL aqui"
 								onChange={getOriginalURL}
 							/>
-							<button className="shortURLButton" type="button">
+							<button className="shortURLButton" type="submit">
 								Encurtar
 							</button>
 						</form>
@@ -34,13 +61,26 @@ function App() {
 					<div className="shrinkedURLContainer">
 						<h2>URL Encurtada:</h2>
 						<div className="shortenedURLContainer">
-							<a href="#">http://localhost:3000</a>
-							<button>Copiar</button>
+							{shortenedURL && (
+								<a
+									className="shortenedURL"
+									href={shortenedURL}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{shortenedURL}
+								</a>
+							)}
 						</div>
 					</div>
 
 					<div className="URLHistoryContainer">
-						<ol>
+						<h2>Histórico de URLs</h2>
+						<ol className="historyList">
+							<li>
+								<a href="">exemplodelink.com</a>
+								<button>copiar</button>
+							</li>
 							<li>
 								<a href="">exemplodelink.com</a>
 								<button>copiar</button>
