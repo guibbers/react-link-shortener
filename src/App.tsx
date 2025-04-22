009f7bfe00066bde18aaa85c70372ddaae52c4ab
Guilherme Torres <guiatorres8@gmail.com>
2025-04-22 01:19:25 -0300

    app done ver 0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../global.css";
 
 function App() {
 	const [originalURL, setOriginalURL] = useState("");
 	const [shortenedURL, setShortenedURL] = useState("");
-	const [linkHistory, setLinkHistory] = useState([]);
+	const [linkHistory, setLinkHistory] = useState<
+		{ original: string; shortened: string; timestamp: string }[]
+	>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	useEffect(() => {
+		console.log("isLoading mudou:", isLoading);
+	}, [isLoading]);
 
 	function getOriginalURL(event: React.ChangeEvent<HTMLInputElement>) {
 		setOriginalURL(event.target.value);
 	}
 
-	async function handleShortenLink(e: Event) {
+	async function handleShortenLink(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		const url = originalURL;
 		const serverURL = "http://localhost:3333/";
+		setIsLoading(true);
 
 		try {
 			const response = await fetch(serverURL, {
@@ -21,10 +28,7 @@ function App() {
 				headers: {
 					"Content-type": "application/json",
 				},
-
-				body: JSON.stringify({
-					url: url,
-				}),
+				body: JSON.stringify({ url }),
 			});
 
 			if (!response.ok) {
@@ -33,9 +37,19 @@ function App() {
 
 			const resultUrl = await response.json();
 			setShortenedURL(resultUrl.shortenedURL);
+
+			setLinkHistory((prev) => [
+				...prev,
+				{
+					original: originalURL,
+					shortened: resultUrl.shortenedURL,
+					timestamp: new Date().toLocaleString(),
+				},
+			]);
 		} catch (error) {
-			console.error(error.message);
+			console.error(error);
 		}
+		setIsLoading(false);
 	}
 
 	return (
@@ -52,8 +66,12 @@ function App() {
 								placeholder="Insira sua URL aqui"
 								onChange={getOriginalURL}
 							/>
-							<button className="shortURLButton" type="submit">
-								Encurtar
+							<button
+								className="shortURLButton"
+								type="submit"
+								disabled={isLoading}
+							>
+								{isLoading ? "Carregando..." : "Encurtar"}
 							</button>
 						</form>
 					</div>
@@ -61,7 +79,9 @@ function App() {
 					<div className="shrinkedURLContainer">
 						<h2>URL Encurtada:</h2>
 						<div className="shortenedURLContainer">
-							{shortenedURL && (
+							{isLoading ? (
+								<div className="spinner"></div>
+							) : shortenedURL ? (
 								<a
 									className="shortenedURL"
 									href={shortenedURL}
@@ -70,21 +90,41 @@ function App() {
 								>
 									{shortenedURL}
 								</a>
-							)}
+							) : null}
 						</div>
 					</div>
 
 					<div className="URLHistoryContainer">
 						<h2>Histórico de URLs</h2>
 						<ol className="historyList">
-							<li>
-								<a href="">exemplodelink.com</a>
-								<button>copiar</button>
-							</li>
-							<li>
-								<a href="">exemplodelink.com</a>
-								<button>copiar</button>
-							</li>
+							{linkHistory.length > 0 ? (
+								linkHistory.map((item, index) => (
+									<li key={index} className="historyItem">
+										<div className="historyLink">
+											<a
+												className="shortenedLinkText"
+												href={item.shortened}
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												{item.shortened}
+											</a>
+											<a
+												className="originalLink"
+												href={item.original}
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												{item.original.substring(0, 20)}{" "}
+												{item.original.length >= 20 && "…"}
+											</a>
+											<p className="timestamp">{item.timestamp}</p>
+										</div>
+									</li>
+								))
+							) : (
+								<p>Nenhum link encurtado ainda.</p>
+							)}
 						</ol>
 					</div>
 				</div>
